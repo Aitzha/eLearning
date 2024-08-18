e7817c8b35448a3edd9b502fcbefe80e6a8d0b43
Gabit Aitzhan <gabit.aitzhan@gmail.com>
2024-08-18 11:02:52 +0500

    Initial commit

---
+++ learning_platform/models.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User, Permission
+from django.db import models
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.TextField(blank=True)
+    permissions = models.ManyToManyField(Permission, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    role = models.ForeignKey(Role, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.user.username
