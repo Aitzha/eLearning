28e2073933bd068e3be906e5da7d7a366ed823ef
Gabit Aitzhan <gabit.aitzhan@gmail.com>
2024-08-26 11:37:17 +0500

    Improve test code

--- learning_platform/tests.py
+++ learning_platform/tests.py
@@ -1,9 +1,11 @@
 from django.urls import reverse
+from django.contrib.auth.models import User
 from rest_framework.test import APITestCase, APIClient
 from rest_framework import status
 from rest_framework.authtoken.models import Token
 
 from .model_factories import UserFactory, UserProfileFactory
+from .models import UserProfile
 
 
 class UserViewAPITest(APITestCase):
@@ -11,15 +13,24 @@ class UserViewAPITest(APITestCase):
         self.client = APIClient()
 
     def test_user_authenticated(self):
+        # Preparation data
         user_profile = UserProfileFactory()
         token = Token.objects.create(user=user_profile.user)
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+
+        # Send request
         response = self.client.get(reverse('api_user'))
+
+        # Check response
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['username'], user_profile.user.username)
+        self.assertEqual(response.data['role'], user_profile.role.name)
 
     def test_user_unauthenticated(self):
+        # Send request
         response = self.client.get(reverse('api_user'))
+
+        # Check response
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
@@ -28,29 +39,54 @@ class ProfileViewTest(APITestCase):
         self.client = APIClient()
 
     def test_profile_authenticated(self):
-        self.user_profile = UserProfileFactory()
-        token = Token.objects.create(user=self.user_profile.user)
+        # Preparation data
+        user_profile = UserProfileFactory()
+        token = Token.objects.create(user=user_profile.user)
         self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+
+        # Send request
         response = self.client.get(reverse('api_profile'))
+
+        # Check the response
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['username'], user_profile.user.username)
+        self.assertEqual(response.data['email'], user_profile.user.email)
+        self.assertEqual(response.data['role'], user_profile.role.name)
 
     def test_profile_unauthenticated(self):
+        # Send request
         response = self.client.get(reverse('api_profile'))
+
+        # Check the response
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
 class RegisterViewTest(APITestCase):
     def test_registration_successful(self):
+        # Preparation data
         data = {
             'username': 'newuser',
             'email': 'newuser@example.com',
             'password': 'newpassword123'
         }
+
+        # Send request
         response = self.client.post(reverse('api_register'), data)
+
+        # Check the response
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
+        # Check Database
+        user = User.objects.get(username=data['username'])
+        user_profile = UserProfile.objects.get(user=user)
+        self.assertNotEquals(None, user)
+        self.assertNotEquals(None, user_profile)
+
     def test_registration_unsuccessful(self):
+        # Send reqeust
         response = self.client.post(reverse('api_register'))
+
+        # Check response
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
 
@@ -61,19 +97,29 @@ class LoginViewTest(APITestCase):
         self.client = APIClient()
 
     def test_login_successful(self):
+        # Preparation data
         data = {
             'username': self.user.username,
             'password': self.password
         }
+
+        # Send request
         response = self.client.post(reverse('api_login'), data)
+
+        # Check response
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_login_unsuccessful(self):
+        # Preparation data
         data = {
             'username': self.user.username,
             'password': 'wrongpassword'
         }
+
+        # Send request
         response = self.client.post(reverse('api_login'), data)
+
+        # Check response
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
