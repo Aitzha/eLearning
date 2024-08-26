9a22b311c4fea28643b426892001b24bc885f531
Gabit Aitzhan <gabit.aitzhan@gmail.com>
2024-08-26 14:45:07 +0500

    Fix profile html - Add profile js code

---
+++ learning_platform/static/js/profile.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const token = localStorage.getItem('token');
+    if (token) {
+        fetch('/api/profile/', {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Token ' + token,
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('username').textContent = data.username;
+            document.getElementById('full-name').textContent = 'Name: ' + data.first_name + ' ' + data.last_name;
+            document.getElementById('email').textContent = 'Email: ' + data.email;
+
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    }
+});
