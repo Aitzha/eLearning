18029f06e1e96f9813357bd1c7b17882d66bc733
Gabit Aitzhan <gabit.aitzhan@gmail.com>
2024-08-26 15:05:21 +0500

    Move login js code to login.js file

--- learning_platform/static/js/app.js
+++ learning_platform/static/js/app.js
@@ -1,4 +1,3 @@
-// Handle header
 document.addEventListener('DOMContentLoaded', function() {
     const userMenu = document.getElementById('user-menu');
     const token = localStorage.getItem('token');
@@ -27,32 +26,3 @@ function logout() {
     localStorage.removeItem('token');
     window.location.href = '/login';
 }
-
-
-// Handle Login
-document.getElementById('login-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-    fetch('/api/login/', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username, password })
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data.token) {
-            localStorage.setItem('token', data.token);
-            window.location.href = '/';
-        } else {
-            alert('Login failed. Please check your credentials and try again.');
-        }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        alert('Login failed. Please try again.');
-    });
-});
