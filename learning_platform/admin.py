4898525a8a311cc9a9becbb255c124e756571733
Gabit Aitzhan <gabit.aitzhan@gmail.com>
2024-09-03 17:54:27 +0500

    Register Course model for admin

--- learning_platform/admin.py
+++ learning_platform/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Role, UserProfile
+from .models import *
 
 admin.site.register(Role)
 admin.site.register(UserProfile)
+admin.site.register(Course)
